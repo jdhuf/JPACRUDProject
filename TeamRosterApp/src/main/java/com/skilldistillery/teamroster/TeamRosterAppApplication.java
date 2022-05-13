2a1e5cd36e6ca226b7208210fe63c2fa6d906052
jdhuf <91219279+jdhuf@users.noreply.github.com>
2022-05-13 16:50:24 -0600

    created boot mvc project, added JPA project dependency

---
+++ TeamRosterApp/src/main/java/com/skilldistillery/teamroster/TeamRosterAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.teamroster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamRosterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamRosterAppApplication.class, args);
+	}
+
+}
---
+++ TeamRosterApp/src/test/java/com/skilldistillery/teamroster/TeamRosterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.teamroster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamRosterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
