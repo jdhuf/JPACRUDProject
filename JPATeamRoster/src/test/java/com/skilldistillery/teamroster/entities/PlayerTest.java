b75e6ef7f2e64ba7372576c09d183c05ec1df389
jdhuf <91219279+jdhuf@users.noreply.github.com>
2022-05-14 08:59:29 -0600

    completed mapping test for Player entity

--- JPATeamRoster/src/test/java/com/skilldistillery/teamroster/entities/PlayerTest.java
+++ JPATeamRoster/src/test/java/com/skilldistillery/teamroster/entities/PlayerTest.java
@@ -10,48 +10,46 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import net.bytebuddy.matcher.ElementMatcher;
 
 class PlayerTest {
-	
+
 	private static EntityManagerFactory emf;
 	private EntityManager em;
 	private Player player;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		
 		emf = Persistence.createEntityManagerFactory("JPATeamRoster");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
 		emf.close();
-		
 	}
-	
-	
 
 	@BeforeEach
 	void setUp() throws Exception {
-		
 		em = emf.createEntityManager();
-		player = em.find(Player.class, 1);
+		player = em.find(Player.class, 4);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
-		
 		em.close();
 		player = null;
 	}
 
 	@Test
-	void test() {
+	@DisplayName("testing basic player mappings")
+	void test1() {
 		assertNotNull(player);
-		assertEquals("Joe Shmow", player.getName());
+		assertEquals("Robbie Robinson", player.getName());
+		assertEquals("17 Dec 1998", player.getBirthdate());
+		assertEquals(74, player.getHeight());
+		assertEquals(170, player.getWeight());
 	}
 
 }
