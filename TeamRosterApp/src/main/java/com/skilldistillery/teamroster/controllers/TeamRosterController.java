9b5ead0dbef8b71a130cc9395f0ccf8becaa68b6
jdhuf <91219279+jdhuf@users.noreply.github.com>
2022-05-14 09:26:16 -0600

    added css folder and main.css, added film folder and show.jsp, added bootstrapFooter.jsp, bootstrapHeader.jsp, added mapping to TeamRosterController

--- TeamRosterApp/src/main/java/com/skilldistillery/teamroster/controllers/TeamRosterController.java
+++ TeamRosterApp/src/main/java/com/skilldistillery/teamroster/controllers/TeamRosterController.java
@@ -4,22 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.skilldistillery.teamroster.data.TeamRosterDAO;
+import com.skilldistillery.teamroster.entities.Player;
 @Controller
 public class TeamRosterController {
 	
 	@Autowired
-	private TeamRosterDAO dao;
+	private TeamRosterDAO teamRosterDao;
 	
 	
 	@RequestMapping(path = {"/", "home.do"})
 	public String index(Model model) {
 		
-		model.addAttribute("DEBUG", dao.findById(1));
-		
-		
+		model.addAttribute("DEBUG", teamRosterDao.findById(1));
+			
 		return "index";
+	}
+	
+	@RequestMapping("getFilm.do")
+	public String showFilm(@RequestParam Integer pid, Model model) {
+		Player player = teamRosterDao.findById(pid);
+				
+				
+		model.addAttribute("player", player);
+		return "player/show";
 	}
 
 }
